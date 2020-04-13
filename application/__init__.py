b2604dd9dd403b47abb1bd7be62d89a965bbd169
JasonJafariHome <jason.jafari.m@gmail.com>
2020-04-13 03:25:52 -0400

    config project

---
+++ application/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+
+def index():
+    return "<h1> Hello Jason!!ss</h1>"
+
+
+
+ 
--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-@app.route("/index")
-
-def index():
-    return "<h1> Hello Jason!</h1>"
-
- 
+from application import app 
